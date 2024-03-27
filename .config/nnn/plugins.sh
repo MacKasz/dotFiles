3b665c15366961c6de62e1c4a8b21dcfc3d56ef8
MacKasz <50265948+MacKasz@users.noreply.github.com>
2024-03-27 22:05:26 +0000

    Making single source script for nnn

--- .config/nnn/plugins.sh
+++ .config/nnn/plugins.sh
@@ -1,5 +1,4 @@
 NNN_PLUG="n:-!wl-copy \"\$nnn\"*"
-NNN_PLUG="$NNN_PLUG;p:-!wl-copy \$(readlink -f \"\$nnn\")*"
-NNN_PLUG="$NNN_PLUG;x:-trash"
+NNN_PLUG="$NNN_PLUG;p:copy-path"
 NNN_PLUG="$NNN_PLUG;i:preview_tui"
 export NNN_PLUG=$NNN_PLUG
